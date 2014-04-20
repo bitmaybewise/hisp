8cbfb54d091693468f69d232ae94e9eae7faf844
Hercules Merscher <hlmerscher@gmail.com>
2014-04-20 11:55:17 -0300

    evaluation

--- parsing.c
+++ parsing.c
@@ -27,6 +27,43 @@ void add_history(char* unused) {}
 
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) {
+    return x + y;
+  }
+  if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) {
+    return x - y;
+  }
+  if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) {
+    return x * y;
+  }
+  if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) {
+    return x / y;
+  }
+
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  /* if tagged as number return it directly, otherwise expression */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* the operator is always second child */
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char **argv) {
   /* Create some parsers */
   mpc_parser_t *Number   = mpc_new("number");
@@ -37,16 +74,16 @@ int main(int argc, char **argv) {
 
   /* Define them with the following Language */
   mpca_lang(MPC_LANG_DEFAULT,
-      "                                                             \
-      number   : /-?[0-9]+.?[0-9]*/ ;                               \
-      operator : '+' | '-' | '*' | '/' | '%'  ;                     \
-      function : /add|sub|mul|div/ ;                                \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;            \
-      hisp     : /^/ <operator> <expr>+ /$/  | <function> <expr>* ; \
+      "                                                                               \
+      number   : /-?[0-9]+/ ;                                                         \
+      operator : '+' | '-' | '*' | '/' | '%'  ;                                       \
+      function : /add|sub|mul|div/ ;                                                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' | '(' <function> <expr>+ ')' ; \
+      hisp     : /^/ <operator> <expr>+ /$/  | /^/ <function> <expr>+ /$/ ;           \
       ",
       Number, Operator, Function, Expr, Hisp);
 
-  puts("Hercules Lisp Version 0.0.0.0.1");
+  puts("Hercules Lisp Version 0.0.0.0.3");
   puts("Press Ctrl+c to Exit\n");
 
   while(1) {
@@ -57,7 +94,8 @@ int main(int argc, char **argv) {
     mpc_result_t r;
     if(mpc_parse("<stdin>", input, Hisp, &r)) {
       /* On success print the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise print the error */
